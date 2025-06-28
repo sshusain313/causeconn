5e6a36b4ad6263f5ffb12369ec1799f6001ba5d0
Tajbir Hossain <mdtajbirhossain031@gmail.com>
2025-06-28 19:40:15 +0600

    Updated autoaxio

--- src/utils/authAxios.ts
+++ src/utils/authAxios.ts
@@ -1,13 +1,19 @@
 import axios from 'axios';
 
-const authAxios = axios.create();
+const authAxios = axios.create({
+  baseURL: import.meta.env.PROD ? 'https://api.changebag.org' : 'http://localhost:5000',
+  withCredentials: true,
+  headers: {
+    'Content-Type': 'application/json'
+  }
+});
 
 authAxios.interceptors.request.use((config) => {
   const token = localStorage.getItem('token');
   if (token && config.headers) {
     config.headers.Authorization = `Bearer ${token}`;
   }
-  config.withCredentials = true;
+  // config.withCredentials = true;
   return config;
 });
 
